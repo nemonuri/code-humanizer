049283fd03e8a0dee3de09dc740fc4ee0f374782
nemonuri <nemonuri2@gmail.com>
2025-06-27 08:51:03 +0900

    불필요한 주석 코드 제거 및 WithTrivia 메서드 정리

--- src/study/Nemonuri.Study.CSharpSyntaxRewriter1/TriviaAdder.cs
+++ src/study/Nemonuri.Study.CSharpSyntaxRewriter1/TriviaAdder.cs
@@ -8,32 +8,6 @@ internal class TriviaAdder : CSharpSyntaxRewriter
     public override SyntaxNode? VisitExpressionStatement(ExpressionStatementSyntax node)
     {
         return WithTrivia(base.VisitExpressionStatement(node));
-
-        /*
-                string triviaChunk = newNode.GetTrailingTrivia().ToFullString();
-                string[] lineSplitted = RegexTheory.GetLineBreakRegex().Split(triviaChunk);
-
-                if (lineSplitted.Length <= 0) { return null; }
-
-                string insertingTrivia = lineSplitted[^1] + "//";
-
-                string[] newSplitted = new string[lineSplitted.Length];
-                for (int i = 0; i < newSplitted.Length; i++)
-                {
-                    if (i == newSplitted.Length - 1)
-                    {
-                        newSplitted[i] = lineSplitted[^1];
-                    }
-                    else if (i == newSplitted.Length - 2)
-                    {
-                        newSplitted[i] = insertingTrivia;
-                    }
-                    else
-                    {
-                        newSplitted[i] = lineSplitted[i];
-                    }
-                }
-        */
     }
 
     public override SyntaxNode? VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
@@ -49,10 +23,6 @@ internal class TriviaAdder : CSharpSyntaxRewriter
     private static SyntaxNode? WithTrivia(SyntaxNode? node)
     {
         if (node is null) { return null; }
-        return node
-                    .WithLeadingTrivia(SyntaxFactory.SyntaxTrivia(SyntaxKind.SingleLineCommentTrivia, "//"))
-                    //.WithTrailingTrivia(SyntaxFactory.SyntaxTrivia(SyntaxKind.WhitespaceTrivia, Environment.NewLine + Environment.NewLine))
-                    ;
-                    
+        return node.WithLeadingTrivia(SyntaxFactory.SyntaxTrivia(SyntaxKind.SingleLineCommentTrivia, "//"));
     }
 }
