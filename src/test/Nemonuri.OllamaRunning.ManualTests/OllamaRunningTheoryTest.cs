102212dcd18ca2a1c744571d916e352081d437d6
nemonuri <nemonuri2@gmail.com>
2025-08-20 13:53:14 +0900

    Console.ReadLine() 이 그냥 null 로 넘어가 버리네...왜지?

--- src/test/Nemonuri.OllamaRunning.ManualTests/OllamaRunningTheoryTest.cs
+++ src/test/Nemonuri.OllamaRunning.ManualTests/OllamaRunningTheoryTest.cs
@@ -1,6 +1,5 @@
 namespace Nemonuri.OllamaRunning.ManualTests;
 
-
 public class OllamaRunningTheoryTest : IClassFixture<OllamaRunningTheoryTestEntryFixture>
 {
     public static bool EnableManualTest => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MANUAL_TEST"));
@@ -10,7 +9,7 @@ public class OllamaRunningTheoryTest : IClassFixture<OllamaRunningTheoryTestEntr
     Set environment variable "MANUAL_TEST" to non-empty string.
     """;
 
-    private readonly ITestOutputHelper _testOutput;
+    private readonly ITestOutputHelper _output;
     private readonly IReadOnlyOllamaRunningTheoryTestEntryFixture _entryFixture;
 
     public OllamaRunningTheoryTest
@@ -19,21 +18,35 @@ public class OllamaRunningTheoryTest : IClassFixture<OllamaRunningTheoryTestEntr
         OllamaRunningTheoryTestEntryFixture entryFixture
     )
     {
-        _testOutput = testOutput;
+        _output = testOutput;
         _entryFixture = entryFixture;
     }
 
     [Fact]
     public void Dummy()
     {
-        _testOutput.WriteLine($"{nameof(_entryFixture)} = {_entryFixture}");
+        _output.WriteLine($"{nameof(_entryFixture)} = {_entryFixture}");
     }
 
     [Fact(Skip = ManualTestDisabled, SkipUnless = nameof(EnableManualTest))]
-    public void CheckLocalOllamaServerRunningStateAsync()
+    public async Task CheckLocalOllamaServerRunningStateAsync()
     {
         // Arrange
+        Console.WriteLine("Please do these:");
+        Console.WriteLine($"1. Check '{_entryFixture.ValidLocalOllamaHostCommand}' is correct local Ollama host command.");
+        Console.WriteLine($"2. Check local Ollama server is not running.");
+        Console.WriteLine("Type 'y' and press 'Enter' to continue..");
+        Assert.Equal("y", Console.ReadLine());
 
         // Act
+        ValueOrErrorMessage<LocalOllamaServerRunningState> actual = await OllamaRunningTheory.CheckLocalOllamaServerRunningStateAsync
+        (
+            localOllamaHostCommand: _entryFixture.ValidLocalOllamaHostCommand,
+            cancellationToken: TestContext.Current.CancellationToken
+        );
+
+        // Assert
+        _output.WriteLine(actual.ToString());
+        Assert.Equal(LocalOllamaServerRunningState.Idle, actual.AsValueOrDefault);
     }
 }
